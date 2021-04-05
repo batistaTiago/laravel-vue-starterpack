9ec0f8ab0ce7dd5ce6662fa8697ec72e91e235ac
tiago <tiago.sousa@arenadunas.com.br>
2021-04-05 18:22:20 -0300

    fix setup

--- initial-setup.sh
+++ initial-setup.sh
@@ -4,6 +4,6 @@ docker-compose up -d;
 
 
 docker-compose exec php bash -c "composer install";
-docker-compose exec php bash -c "npm install";
 docker-compose exec php bash -c "service cron start";
+docker-compose exec php bash -c "mv .env.example .env";
 docker-compose exec php bash -c "php artisan key:generate";
